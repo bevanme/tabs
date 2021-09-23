8e573097e24fa55782b37818c63815a3b60a5053
Gail_Mitchell <gail_mitchell@dell.com>
2021-09-23 00:17:16 -0500

    completed tabs

--- tabs.js
+++ tabs.js
@@ -1,1 +1,23 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(e) {
+   const thisTab = e.currentTarget;
+   const matchingPanel = e.currentTarget.id;
+   
+   // hide all tab panels
+   tabPanels.forEach(panel => panel.hidden = true);
+   // mark all tabs as unselected
+   tabButtons.forEach(tab => tab.setAttribute('aria-selected', false));
+   // mark this tab as selected
+   thisTab.setAttribute('aria-selected', true);   
+   // show this tab's panel
+   //const thisTabPanel = tabs.querySelector(`[aria-labelledby="${matchingPanel}"]`);
+   const thisTabPanel = tabPanels.find(panel => 
+      panel.getAttribute('aria-labelledby') === matchingPanel);
+   thisTabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => 
+    button.addEventListener('click', handleTabClick));
